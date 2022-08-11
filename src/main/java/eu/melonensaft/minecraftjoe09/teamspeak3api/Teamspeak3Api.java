ad445fbe55a63065bf3c9e904fb110dfa0bfe3e9
MinecraftJoe09 <portschy.alexander@gmail.com>
2022-08-11 21:27:10 +0200

    Commit

---
+++ src/main/java/eu/melonensaft/minecraftjoe09/teamspeak3api/Teamspeak3Api.java
@@ -0,0 +1,17 @@
+package eu.melonensaft.minecraftjoe09.teamspeak3api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Teamspeak3Api extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
